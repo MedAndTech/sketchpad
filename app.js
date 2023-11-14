afedd85c70c1534c3da9b20126400c4bd57f40f1
MedAndTech <122757488+MedAndTech@users.noreply.github.com>
2023-11-14 01:25:33 +0100

    Create a webpage with a 16x16 grid of square divs

---
+++ app.js
@@ -0,0 +1,8 @@
+const grid = document.querySelector("#grid")
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
+    }
+}
