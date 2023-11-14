df3b28b1a3a999917ad405e02ab001e5a46237da
MedAndTech <122757488+MedAndTech@users.noreply.github.com>
2023-11-14 02:27:09 +0100

    Add mouseover event listener to squares and colorSquare()

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ function createGrid(size) {
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const div = document.createElement("div");
+            div.addEventListener("mouseover", () => {
+                getPenColor(div);
+            })
             div.style.width = GRID_SIZE / size + "px";
             div.style.height = GRID_SIZE / size + "px";
             div.classList.add("square");
@@ -21,6 +24,12 @@ function resetGrid() {
     }
 }
 
+function getPenColor(elem) {
+    if (mouseDown) {
+        elem.style.backgroundColor = color;
+    }
+}
+
 
 const grid = document.querySelector(".grid")
 
@@ -29,5 +38,12 @@ sizeButton.addEventListener("click", () => {
     let gridSize = +prompt("Number of square per size (min 16, max 100)");
     createGrid(gridSize);
 })
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+let color = "black";
+
+
 
 createGrid(16);
